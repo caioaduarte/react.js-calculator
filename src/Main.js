4dc1f40cfe57c2123b8bd568c3932ac3ed5f65f6
Caio Duarte <caio1903@gmail.com>
2021-09-21 04:13:31 -0300

    Code formatting

--- src/Main.js
+++ src/Main.js
@@ -27,170 +27,40 @@ function Main() {
               <input type="text" value={inputvalue} />
             </td>
             <td>
-              <button
-                onClick={() => {
-                  clearEverything();
-                }}
-              >
+              <button onClick={() => {clearEverything();}}>
                 CE
               </button>
             </td>
           </tr>
 
           <tr>
-            <td>
-              <button
-                onClick={() => {
-                  display("1");
-                }}
-              >
-                1
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("2");
-                }}
-              >
-                2
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("3");
-                }}
-              >
-                3
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("/");
-                }}
-              >
-                /
-              </button>
-            </td>
+            <td><button onClick={() => {display("1");}}>1</button></td>
+            <td><button onClick={() => {display("2");}}>2</button></td>
+            <td><button onClick={() => {display("3");}}>3</button></td>
+            <td><button onClick={() => {display("/");}}>/</button></td>
           </tr>
 
           <tr>
-            <td>
-              <button
-                onClick={() => {
-                  display("4");
-                }}
-              >
-                4
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("5");
-                }}
-              >
-                5
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("6");
-                }}
-              >
-                6
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("-");
-                }}
-              >
-                -
-              </button>
-            </td>
+            <td><button onClick={() => {display("4");}}>4</button></td>
+            <td><button onClick={() => {display("5");}}>5</button></td>
+            <td><button onClick={() => {display("6");}}>6</button></td>
+            <td><button onClick={() => {display("-");}}>-</button></td>
           </tr>
+
           <tr>
-            <td>
-              <button
-                onClick={() => {
-                  display("7");
-                }}
-              >
-                7
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("8");
-                }}
-              >
-                8
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("9");
-                }}
-              >
-                9
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("+");
-                }}
-              >
-                +
-              </button>
-            </td>
+            <td><button onClick={() => {display("7");}}>7</button></td>
+            <td><button onClick={() => {display("8");}}>8</button></td>
+            <td><button onClick={() => {display("9");}}>9</button></td>
+            <td><button onClick={() => {display("+");}}>+</button></td>
           </tr>
+
           <tr>
-            <td>
-              <button
-                onClick={() => {
-                  display(".");
-                }}
-              >
-                .
-              </button>
-            </td>
-            <td>
-              {" "}
-              <button
-                onClick={() => {
-                  display("0");
-                }}
-              >
-                0
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  calculate();
-                }}
-              >
-                =
-              </button>
-            </td>
-            <td>
-              <button
-                onClick={() => {
-                  display("*");
-                }}
-              >
-                *
-              </button>
-            </td>
+            <td><button onClick={() => {display(".");}}>.</button></td>
+            <td><button onClick={() => {display("0");}}>0</button></td>
+            <td><button onClick={() => {calculate();}}>=</button></td>
+            <td><button onClick={() => {display("*");}}>*</button></td>
           </tr>
+
         </table>
       </div>
     </div>
